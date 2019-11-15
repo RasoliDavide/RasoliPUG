a4c8aaa80fdae58234b7275e5a62b2195d8373eb
RasoliDavide <rasoli.davide@iisgalvanimi.edu.it>
2019-11-15 13:00:06 +0100

    xcv

--- PUG_Lego/app.js
+++ PUG_Lego/app.js
@@ -1,17 +1,26 @@
 var express = require('express');
 var app = express();
-const lego = require('./json/lego.json');
+const legoj = require('./json/lego.json');
+
+
+
 app.use(express.static(__dirname + '/public')); 
-app.set('view engine', 'pug');  
+app.set('view engine', 'pug'); 
+
+
 app.listen(3000, function()
 {
     console.log("Server avviato sulla porta 3000");
+    
 });
+
 app.get('/', function(req, res)
 {
-    res.render('index.pug', lego);
+    res.render('index.pug', {legos : legoj.lego});
 });
+
 app.get('/lego', function(req, res){
-    const mod = lego.lego.find((l) => l.SetNumber === req.query.n);
-    res.render('instruction', mod);
+    const mods = legoj.lego.find(l => l.SetNumber === req.query.n);
+    console.log(mods);
+    res.render('instruction', {mod : mods});
 });
